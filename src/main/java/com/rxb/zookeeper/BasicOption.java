75b2ebcd2c7d2cd4712a75b2d5b44e5cace164a0
ren.xiaobo <ren.xiaobo@jyall.com>
2018-04-23 18:39:11 +0800

    remove useless import

--- src/main/java/com/rxb/zookeeper/BasicOption.java
+++ src/main/java/com/rxb/zookeeper/BasicOption.java
@@ -6,8 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 /**
  * Created by ren.xiaobo on 2018/4/23.
